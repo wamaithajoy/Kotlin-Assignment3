5f5495dcddbdf3e7adef92f966b8efb9473bebfc
wamaithajoy <joyywamaitha@gmail.com>
2022-03-09 13:16:12 +0300

    third commit

---
+++ src/main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    namesofGirls(arrayOf("Valary","Eunice","Millie","Sophy"))
+    cities()
+    numeric()
+    detail(arrayOf("Vallary","Gitu","Heta"))
+
+
+}
+
+
+
+
+fun namesofGirls(names:Array<String>){
+    println(names.contentToString())
+
+    }
+fun cities(){
+ var cities=arrayOf("harare","mumbai","dodoma","jakarta")
+    cities.forEach{cities->
+        println(cities.capitalize())
+
+    }
+}
+fun numeric(){
+    var numbers=arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+    var sum=numbers[2]+numbers[5]
+    println(sum)
+    var index=numbers.indexOf(158)
+println(index)
+    var sortednumbers=numbers.sortedArray()
+    println(sortednumbers.contentToString())
+}
+
+fun detail(name:Array<String>):Array<String>{
+    var names= name
+    println(names.contentToString())
+    return names
+}
+
